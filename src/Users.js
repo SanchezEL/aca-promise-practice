03f90e6dc7043a06c019f6093757b48f0b15ec95
Emilio Sanchez <sanchez.emilio483@gmail.com>
2019-05-28 20:40:52 -0500

    initial push

--- src/Users.js
+++ src/Users.js
@@ -1,7 +1,15 @@
 import React, { Component, Fragment } from 'react'
 import { Button } from '@material-ui/core'
+import { connect } from 'react-redux'
+import {setUsers, getUsersFromAPI} from './actions'
 
 class Users extends Component {
+  // state = {
+  //   users: []
+  // }
+  getUsers = () =>{
+    this.props.getUsersFromAPI()
+  }
   render() {
     return (
       <Fragment>
@@ -10,11 +18,31 @@ class Users extends Component {
           <Button onClick={this.getUsers} variant="contained" color="primary">Get Users</Button>
         </div>
         <div className="users-block">
-          {/* Map through users here */}
+          {/* Map through users here */
+            this.props.users.map((user) =>(
+              <div>
+                <p><b>Name: {user.name}</b></p>
+                <p>Username: {user.username}</p>
+                <p>Email: {user.email}</p>
+                <p>Website: {user.website}</p>
+              </div>
+            ))
+        }
         </div>
       </Fragment>
     )
   }
 }
+const mapStateToProps = (state) => {
+  return {
+    users: state.users
+  }
+}
+const mapDispatchToProps = dispatch =>{
+  return {
+    getUsersFromAPI: () => dispatch(getUsersFromAPI())
+    // setUsers: (users) => dispatch(setUsers(users))
+  }
+}
 
-export default Users
+export default connect(mapStateToProps, mapDispatchToProps)(Users)
